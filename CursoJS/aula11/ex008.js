53a55974a182ddbf039dabbebb109a2e333e8d61
WillamesSilv <62750897+WillamesSilv@users.noreply.github.com>
2020-06-22 21:41:39 -0300

    exercícios JS

--- CursoJS/aula11/ex008.js
+++ CursoJS/aula11/ex008.js
@@ -3,4 +3,4 @@ console.log(`A velocidade do seu carro é ${vel}km/h`)
 if (vel > 60) { //condição simples
     console.log(`Você ultrapassou o limite de velocidade. MULTADO!`)
 }
-console.log('Diriga sempre usando o cinto de segurança!.')
+console.log('Dirija sempre usando o cinto de segurança!.')
