f2d2fa7341d541dd4904794a5eb002662430878a
WillamesSilv <62750897+WillamesSilv@users.noreply.github.com>
2020-06-24 23:39:54 -0300

    Update ex011.js

--- CursoJS/aula12/ex011.js
+++ CursoJS/aula12/ex011.js
@@ -1,7 +1,7 @@
 var idade = 28
-if (idade <16) {
+if (idade < 16) {
     console.log('Não vota!')
-}else if (idade <18) {
+}else if (idade < 18 || idade > 65) {
     console.log('Voto opcional.')
 }else {
     console.log('Voto obrigatório!')
