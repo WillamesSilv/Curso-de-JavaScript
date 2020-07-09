092af80bad266bb55e8bb2cf36f8c071a25127ed
WillamesSilv <62750897+WillamesSilv@users.noreply.github.com>
2020-07-08 22:59:31 -0300

    tema

--- CursoJS/aula12ex/ex014/script.js
+++ CursoJS/aula12ex/ex014/script.js
@@ -7,12 +7,17 @@ function carregar () {
     if (hora >= 0 && hora < 12){
         // BOM DIA!
         img.src = 'manha.png'
+        document.body.style.backgroundColor = '#beac9e'
+
     } else if (hora >= 12 && hora < 18) {
         // BOA TARDE!
         img.src = 'tarde.png'
+        document.body.style.backgroundColor = '#f06101'
+
     } else {
         // BOA NOITE!
         img.src = 'noite.png'
+        document.body.style.backgroundColor = '#0c1126'
 
     }
 
