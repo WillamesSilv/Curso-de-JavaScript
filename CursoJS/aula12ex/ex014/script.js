5e524d2fa434dee7a6241080408464ea7de0402b
WillamesSilv <62750897+WillamesSilv@users.noreply.github.com>
2020-07-08 22:40:52 -0300

    script de horas

--- CursoJS/aula12ex/ex014/script.js
+++ CursoJS/aula12ex/ex014/script.js
@@ -3,6 +3,18 @@ function carregar () {
     const img = document.getElementById('foto')
     const data = new Date()
     const hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12){
+        // BOM DIA!
+        img.src = 'manha.png'
+    } else if (hora >= 12 && hora < 18) {
+        // BOA TARDE!
+        img.src = 'tarde.png'
+    } else {
+        // BOA NOITE!
+        img.src = 'noite.png'
+
+    }
 
 }
 
