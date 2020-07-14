b14f65171c5de838e024dd103a504af913cc7a4e
WillamesSilv <62750897+WillamesSilv@users.noreply.github.com>
2020-07-14 00:51:07 -0300

    Update script.js

--- CursoJS/aula12ex/ex0015/script.js
+++ CursoJS/aula12ex/ex0015/script.js
@@ -1,8 +1,8 @@
 function verificar() {
-    var data = new Date();
-    var ano = data.getFullYear()
-    var fano = document.getElementById('txtano')
-    var res = document.getElementById('res')
+    var data = new Date();//data atual
+    var ano = data.getFullYear()//ano atual, fullYear o ano com 4 digitos
+    var fano = document.getElementById('txtano')//pegando o dado do input
+    var res = document.getElementById('res')//selecionando a div
     if (fano.value.length == 0 || Number(fano.value) > ano) {
         alert('[ERRO!!] Verifique os dados e tente novamente.')
     } else {
