794d1143908799d0a640fb8c3200c12ebd60b7c1
Zinho <62750897+WillamesSilv@users.noreply.github.com>
2020-09-15 16:19:02 -0300

    ex017

--- CursoJS/aula14ex/ex017/script.js
+++ CursoJS/aula14ex/ex017/script.js
@@ -1,1 +1,23 @@
-alert('Olá, mundo')
+function clicar(){
+  let ntext = document.getElementById('number')
+  let tab = document.getElementById('seltab')
+  let num = Number(ntext.value)
+  if (ntext.value.length == 0) {
+    alert('Erro! Preencha o campo NÚMERO')
+  }else {
+    let c = 1
+    tab.innerHTML=" "
+    while (c <= 10) {
+      let item = document.createElement('option')
+      item.text = `${num} x ${c} = ${num*c}`
+      item.value = `tab${c}`
+      tab.appendChild(item)
+      c++
+    }
+    
+  }
+  
+
+}
+
+
